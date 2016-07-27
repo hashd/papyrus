470579afb273099788517376c751a203b063e17a
dhavalsinhzala <dhaval.zala@live.com>
2016-07-27 15:33:28 +0530

    Add binding between Steps and SVG elements. -Change SVG element selection using tab key. -Change step selection according to SVG element selection.

--- src/dvu/core/step.ts
+++ src/dvu/core/step.ts
@@ -3,6 +3,8 @@ import {Scope} from './scope'
 import {ValueType} from './data/data_definition'
 
 export class Step {
+  private element: Element
+
   constructor(public command: Command, public data: Object) {
 
   }
@@ -10,16 +12,22 @@ export class Step {
   addParameter(name: string, value: ValueType) {
     this.data[name] = value
   }
-  
+
   isValid(): boolean {
     return this.command.validate(this.data)
   }
-  
+
   getSummary() {
     return this.command.getSummary(this.data)
   }
 
+  getElement() {
+    return this.element
+  }
+
   execute(scope: Scope) {
-    return this.command.execute(this.data, scope)
+    let result = this.command.execute(this.data, scope)
+    this.element = result.element
+    return result
   }
 }
