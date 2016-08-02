706927ce32ad80f28df08aa924da1ecd49051f42
Charanjit Singh <charanjit.singh@imaginea.com>
2016-08-02 20:25:54 +0530

    Add select panel functionality

--- src/web/components/core/editor.ts
+++ src/web/components/core/editor.ts
@@ -14,13 +14,21 @@ import { Messages, Subjects } from 'src/web/services/messages'
   template: `
     <div class="row row-no-padding" full-length>
       <div class="col col-md-3 sidebar" full-length>
+    
+         <label>  <input type="checkbox" [(ngModel)]="showhideData" (ngModelChange)="onChange1($event)" /> Data  </label>
+         <label>  <input type="checkbox" [(ngModel)]="showhideStyle" (ngModelChange)="onChange2($event)"/> Style  </label>
+         <label>  <input type="checkbox" [(ngModel)]="showhideMac" (ngModelChange)="onChange3($event)"/> Mac  </label>
+   
         <pa-data [data]="visualization?.data"
           [dataObservables]="visualization?.dataObservables"
           [datasetDefinition]="visualization?.datasetDefinition"
-          [style.height]="'50%'">
+          *ngIf="showhideData"
+          class="panel-height-{{count}}"
+         >
         </pa-data>
-
-        <pa-steps [steps]="visualization?.block?.steps" [visualization]="visualization" [style.height]="'50%'" (selectedStep)="selectStep($event)" (removedStep)="removeStep($event)"></pa-steps>
+        
+        <pa-steps [steps]="visualization?.block?.steps" [visualization]="visualization" *ngIf="showhideStyle" class="panel-height-{{count}}"  (selectedStep)="selectStep($event)" (removedStep)="removeStep($event)"></pa-steps>
+        <pa-steps [steps]="visualization?.block?.steps" [visualization]="visualization" *ngIf="showhideMac" class="panel-height-{{count}}" (selectedStep)="selectStep($event)" (removedStep)="removeStep($event)"></pa-steps>
       </div>
       <div class="col col-md-9 editor" full-length>
         <pa-canvas [currentStep]="selectedStep" [visualization]="visualization" [commands]="commands" full-length></pa-canvas>
@@ -31,10 +39,68 @@ import { Messages, Subjects } from 'src/web/services/messages'
   providers: [CommandService]
 })
 export class PapyrusEditor {
+   showhideData:boolean = true;
+    showhideStyle:boolean = true;
+     showhideMac:boolean = true;
+    count:number=3;
+    height:any;
+
   @Input()
   visualization: CompositeVisualization
   commands: Command[]
   selectedStep: Step
+
+  onChange1(value: boolean) {
+    
+    this.showhideData = value;
+    
+   if(this.showhideData==false && this.showhideStyle==true && this.showhideMac==true){
+   this.count=2;
+   }else if(this.showhideData==true && this.showhideStyle==true && this.showhideMac==false){
+  this.count=2;
+   }else if(this.showhideData==true && this.showhideStyle==false && this.showhideMac==true){
+  this.count=2;
+   }else if(this.showhideData==true && this.showhideStyle==true && this.showhideMac==true){
+  this.count=3;
+   }
+   else{
+     this.count=1;
+   }
+}
+
+  onChange2(value: boolean) {
+    
+    this.showhideStyle = value;
+    
+ if(this.showhideData==true && this.showhideStyle==false && this.showhideMac==true){
+   this.count=2;
+   }else if(this.showhideData==false && this.showhideStyle==true && this.showhideMac==true){
+  this.count=2;
+   }else if(this.showhideData==true && this.showhideStyle==true && this.showhideMac==false){
+  this.count=2;
+   }else if(this.showhideData==true && this.showhideStyle==true && this.showhideMac==true){
+  this.count=3;
+   }else{
+     this.count=1;
+   }
+
+  }
+  onChange3(value: boolean) {
+
+    this.showhideMac = value;
+   
+    if(this.showhideData==true && this.showhideStyle==true && this.showhideMac==false){
+   this.count=2;
+   }else if(this.showhideData==true && this.showhideStyle==false && this.showhideMac==true){
+  this.count=2;
+   }else if(this.showhideData==false && this.showhideStyle==true && this.showhideMac==true){
+  this.count=2;
+   }else if(this.showhideData==true && this.showhideStyle==true && this.showhideMac==true){
+  this.count=3;
+   }else{
+     this.count=1;
+   }
+  }
 
   constructor(private commandService: CommandService) {
     this.commands = commandService.getCommands()
